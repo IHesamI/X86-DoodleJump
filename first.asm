5527b74cec80a66b2045ba8a6c1f55eb67b9065d
hesam <bhesam2000@yahoo.com>
2023-03-30 20:54:26 +0300

    Try to move the ball

--- first.asm
+++ first.asm
@@ -4,33 +4,70 @@ Stack SEGMENT PARA Stack
 Stack ends
 
 DATA segment para 'DATA'
+    TIME_AUX DB 0 ;
+    BALL_X DW 0A0h                       ; current X position (column) of the ball
+	BALL_Y DW 0A0h                       ; current Y position (column) of the ball 
+    BALL_SIZE DW 04H                     ; size of the ball 
+    Ball_Velocity DW 04H
 Data ends
 
 
 code segment para 'CODE'
     main proc fAR
-        ; mov dl ,'A'
-        ; mov ah,6h
-        ; int 21h
+	    ASSUME CS:CODE,DS:DATA,SS:STACK      ;assume as code,data and stack segments the respective registers
+	    PUSH DS                              ;push to the stack the DS segment
+	    SUB AX,AX                            ;clean the AX register
+	    PUSH AX                              ;push AX to the stack
+        MOV AX,DATA                          ;save on the AX register the contents of the DATA segment
+        MOV DS,AX                            ;save on the DS segment the contents of AX
+        POP AX                               ;release the top item from the stack to the AX register
+        POP AX                               ;release the top item from the stack to the AX register
+        mov ah ,0h
+        int 10h                        
 
-        mov ah , 00h
-        mov al , 13h
-        int 10h
+            CHECK_TIME:
+                mov ah,2ch ; GET THE SYSTEM TIME
+                int 21h
+                CMP DL,TIME_AUX
+                je CHECK_TIME
+                MOV TIME_AUX,DL
+                sub BALL_Y,Ball_Velocity
+                CALL DRAW_BALL
+                JMP CHECK_TIME
 
-        mov ah , 0bh
-        mov bh , 00h
-        mov bl , Dh
-        int 10h
+            ret 
 
-        mov ah , 0Ch
-        ; mov al , 07h ; Set the color of pixel
-        mov bh , 00h
-        mov cx , 0Ah
-        mov dx , 0Ah
-        int 10h
-        ret 
     main endp
 
+    DRAW_BALL PROC NEAR:
+        ; mov ah ,7h
+        ; int 10h
+
+        mov ah ,0h
+        int 10h
+
+        mov cx , BALL_X ;initial column 
+        mov dx , BALL_Y ;initial line
+        DRAW_BALL_Horizontal:
+            mov ah , 0Ch
+            mov al , 02h ; Set the color of pixel
+            mov bh , 00h
+            int 10h
+            inc cx
+            MOV ax, cx
+            SUB ax,BALL_X
+            cmp ax,BALL_SIZE
+            jng DRAW_BALL_Horizontal
+            mov cx , BALL_X ;initial column 
+            inc dx 
+            MOV ax, dx
+            SUB ax,BALL_Y
+            cmp ax,BALL_SIZE
+            jng DRAW_BALL_Horizontal
+
+        DRAW_BALL_VERTICAL:
+        RET
+    DRAW_BALL ENDP
 
 code ends
 end
